9eed3c76f546e1df09fd3875e61e30ded769eb98
Arham <k200195@nu.edu.pk>
2024-03-18 22:50:35 +0500

    created Category database and category controller, rendered category list on homepage

---
+++ BookStoreManagmentSystem/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using BookStoreManagmentSystem.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookStoreManagmentSystem.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
